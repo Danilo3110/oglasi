9864ed84dee52cff8d72d1433627421aab16d8d6
darkomingovski <darko.mingovski@gmail.com>
2019-03-15 21:49:19 +0100

    ispravljen sadrzaj oglasa na naslovnoj

--- src/js/main.js
+++ src/js/main.js
@@ -17,12 +17,12 @@ class RealEstate {
 
 const ad1 = new RealEstate('Beograd', 'img/kopernikus1a.jpg', 'Kopernikus panorama, novo, lux', 166000, '063/770-9316');
 const ad2 = new RealEstate('Beograd', 'img/djeram-pijaca-lux-novogradnja-id-2460-5425634537964-71787696524.jpg', 'Novi Beograd, lux, odmah useljivo', 98000, '063/200-405');
-const ad3 = new RealEstate('Zrenjanin', 'img/krovopokrivacki-radovi-5425626382088-71785611462.jpg', 'Popravka svih vrsta krovova', 0, '065/5652-532');
-const ad4 = new RealEstate('Niš', 'img/vodoinstalater-najnize-cene-hitne-intervencij-5425491020580-71782157444.jpg', 'Sve vrste vodoinstalaterskih usluga', 1500, '062/580-965');
+const ad3 = new RealEstate('Beograd', 'img/konjarnik-olge-alkalaj.jpg', 'Konjarnik, Olge Alkalaj, jednoiposoban', 54000, '065/5652-532');
+const ad4 = new RealEstate('Beograd', 'img/dedinje---beli-dvor-8.jpg', 'Beograd, Dedinje', 165000, '062/580-965');
 const ad5 = new RealEstate('Novi Sad', 'img/lux-3-0-stan-garaza-odlicna-ponuda-id-8768-5425634480684-71787728297.jpg', 'Prelep, renovirano, ugradna kuhinja', 71000, '064/1252-596');
-const ad6 = new RealEstate('Šabac', 'img/molersko-farbarski-radovi-5425475228587-71781068721.jpg', 'Sve vrste molersko-farbarskih radova', 0, '060/052-8524');
-const ad7 = new RealEstate('Beograd', 'img/mac-book-air-2017--kao-novi-5425634510378-71787565208.jpg', 'Mac book air 2017, povoljno', 1200, '063/512-856');
-const ad8 = new RealEstate('Čačak', 'img/prodajem-huawei-mate-20-lite-crna-boja-64-gb-slika-108441511.jpg', 'Huawei mate 20 lite, nov', 300, '069/1874-844');
+const ad6 = new RealEstate('Beograd', 'img/novi-beograd.jpg', 'Novi Beograd, Hotel Jugoslavija', 155000, '060/052-8524');
+const ad7 = new RealEstate('Subotica', 'img/subotica---centar-3.jpg', 'Subotica, Centar 3', 38000, '063/512-856');
+const ad8 = new RealEstate('Novi Sad', 'img/odlican-uknjizen.jpg', 'Novi Sad, Novo naselje', 56000, '069/1874-844');
 
 function renderAds() {
     baseOfRealEstate.forEach(function (ad) {
