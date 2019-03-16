f394daa3d755544a244557a71212be7b1397460e
Danilo3110 <lukic3@sbb.rs>
2019-03-16 21:54:04 +0100

    render from base 0.1

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,32 @@
+'use strict';
+
+const serverUrl = `http://localhost:3000`;
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: `${serverUrl}`
 });
+api.defaults.timeout = 2500;
 
-const baseOfRealEstate = [];
-class RealEstate {
-    constructor(id, location, image, description, price, contacts) {
-        this.id = id;
-        this.location = location;
-        this.image = image;
-        this.description = description;
-        this.price = price;
-        this.contacts = contacts;
-        baseOfRealEstate.push(this);
-    };
-};
 
-const ad1 = new RealEstate(1, 'Beograd', 'img/kopernikus1a.jpg', 'Kopernikus panorama, novo, lux', 166000, '063/770-9316');
-const ad2 = new RealEstate(2, 'Beograd', 'img/djeram-pijaca-lux-novogradnja-id-2460-5425634537964-71787696524.jpg', 'Novi Beograd, lux, odmah useljivo', 98000, '063/200-405');
-const ad3 = new RealEstate(3, 'Beograd', 'img/konjarnik-olge-alkalaj.jpg', 'Konjarnik, Olge Alkalaj, jednoiposoban', 54000, '065/5652-532');
-const ad4 = new RealEstate(4, 'Beograd', 'img/dedinje---beli-dvor-8.jpg', 'Beograd, Dedinje', 165000, '062/580-965');
-const ad5 = new RealEstate(5, 'Novi Sad', 'img/lux-3-0-stan-garaza-odlicna-ponuda-id-8768-5425634480684-71787728297.jpg', 'Telep, renovirano, ugradna kuhinja', 71000, '064/1252-596');
-const ad6 = new RealEstate(6, 'Beograd', 'img/novi-beograd.jpg', 'Novi Beograd, Hotel Jugoslavija', 155000, '060/052-8524');
-const ad7 = new RealEstate(7, 'Subotica', 'img/subotica---centar-3.jpg', 'Subotica, Centar 3', 38000, '063/512-856');
-const ad8 = new RealEstate(8, 'Novi Sad', 'img/odlican-uknjizen.jpg', 'Novi Sad, Novo naselje', 56000, '069/1874-844');
-
-function renderAds() {
-    baseOfRealEstate.forEach(function (ad) {
+async function renderAds() {
+    const responseListings = await api.get(`/listings`);
+    const listings = responseListings.data;
+    for (const ad of listings) {
         const $adsContainer = $('.ads-container');
         const $ad = $(`<div class="ads" id="${ad.id}">
                         <div class="ads-descr">
-                            <h3>Lokacija: ${ad.location}<i class="fas fa-share-alt fa-lg"></i><i class="far fa-heart fa-lg"></i><i
-                                class="fas fa-map-marker-alt fa-lg"></i></h3>
+                        <h3>Lokacija: ${ad.city}<i class="fas fa-share-alt fa-lg"></i><i class="far fa-heart fa-lg"></i>
+                        <i class="fas fa-map-marker-alt fa-lg"></i></h3>
                         </div>
-                        <img src="${ad.image}" alt=""><br>
+                        <img src="${ad.imgUrl}" alt=""><br>
                         <h2 class="ads-descr">${ad.description}</h2>
                         <h3 class="ads-descr">cena: ${ad.price.toLocaleString('sr-RS') == 0 ? 'po dogovoru'
-                : ad.price.toLocaleString('sr-RS') + '&euro;'}</h3>
+                                                    : ad.price.toLocaleString('sr-RS') + '&euro;'}</h3>
                         <hr>
                         <h3 class="ads-descr">kontakt: ${ad.contacts}</h3>
                         </div>`);
         $ad.appendTo($adsContainer);
         $(`#${ad.id}`).on('click', () => fullAds(ad.id));
-    });
+    }
 };
 
 function renderFullAds() {
