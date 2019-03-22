b6c48ad99b86b2b6d6bffa097fa24dd0e8f36237
Danilo3110 <lukic3@sbb.rs>
2019-03-22 16:18:28 +0100

    small search

--- src/js/main.js
+++ src/js/main.js
@@ -270,40 +270,50 @@ async function searchAds() {
     const response = await api.get(`/listings`);
     const listingsDb = response.data;
     const filteredAds = [];
-    const advancedFiltered = [];
+    // const advancedFiltered = [];
     for (const i in listingsDb) {
         if ((listingsDb[i].title).includes($searchKey) && listingsDb[i].city == $searchCity && listingsDb[i].category == $searchCat) {
             filteredAds.push(listingsDb[i]);
         }
     }
-    $("#advancedSearch").find("input:not(:checkbox), input:checked, select").each(function () {
+    /*
+    $('#advancedSearch').find('input:checked, select').each(function () {
         if (!($(this).val() === '' || $(this).val() === null)) {
             advancedFiltered.push($(this).val());
         }
     });
+
+    for (let b in filteredAds) {
+        let adObj = JSON.stringify(filteredAds[b]);
+        for (let c in advancedFiltered) {
+            if (!(adObj.includes(advancedFiltered[c]))) {
+                filteredAds.splice(c, 1);
+            }
+        }
+    }
     console.log(filteredAds);
-    console.log(advancedFiltered);
-    //     $('.ads-container').html('');
-    //     $('.ads-click-scroll').html('Rezultati pretrage:');
-    //     for (const ad of advancedFiltered) {
-    //         const responseUsers = await api.get(`/users/${ad.authorId}`);
-    //         const users = responseUsers.data;
-    //         const $adsContainer = $('.ads-container');
-    //         const $ad = $(`<div class="ads" id="${ad.id}_searched">
-    //                         <div class="ads-descr">
-    //                         <h3>Lokacija: ${ad.city}<i class="fas fa-share-alt fa-lg"></i><i class="far fa-heart fa-lg"></i>
-    //                         <i class="fas fa-map-marker-alt fa-lg"></i></h3>
-    //                         </div>
-    //                         <img src="${ad.imgUrl[0]}" alt=""><br>
-    //                         <h2 class="ads-descr">${ad.title}</h2>
-    //                         <h3 class="ads-descr">cena: ${Number(ad.price).toLocaleString('sr-RS') == null ? ad['price-other']
-    //                                                     : Number(ad.price).toLocaleString('sr-RS') + '&euro;'}</h3>
-    //                         <hr>
-    //                         <h3 class="ads-descr">kontakt: ${users.mobile}</h3>
-    //                         </div>`);
-    //         $ad.appendTo($adsContainer);
-    //         $(`#${ad.id}_searched`).on('click', () => fullAds(ad.id));
-    //     }
+    console.log(advancedFiltered);*/
+    $('.ads-container').html('');
+    $('.ads-click-scroll').html('Rezultati pretrage:');
+    for (const ad of filteredAds) {
+        const responseUsers = await api.get(`/users/${ad.authorId}`);
+        const users = responseUsers.data;
+        const $adsContainer = $('.ads-container');
+        const $ad = $(`<div class="ads" id="${ad.id}_searched">
+                    <div class="ads-descr">
+                    <h3>Lokacija: ${ad.city}<i class="fas fa-share-alt fa-lg"></i><i class="far fa-heart fa-lg"></i>
+                    <i class="fas fa-map-marker-alt fa-lg"></i></h3>
+                    </div>
+                    <img src="${ad.imgUrl[0]}" alt=""><br>
+                    <h2 class="ads-descr">${ad.title}</h2>
+                    <h3 class="ads-descr">cena: ${Number(ad.price).toLocaleString('sr-RS') == null ? ad['price-other']
+                                                : Number(ad.price).toLocaleString('sr-RS') + '&euro;'}</h3>
+                    <hr>
+                    <h3 class="ads-descr">kontakt: ${users.mobile}</h3>
+                    </div>`);
+        $ad.appendTo($adsContainer);
+        $(`#${ad.id}_searched`).on('click', () => fullAds(ad.id));
+    }
 };
 
 function eventsAll() {
